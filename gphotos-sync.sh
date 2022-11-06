a0f39b02a1eef8349b384998975971dc83ce9f5f
Iago Lopez Galeiras <iaguis@gmail.com>
2022-11-06 16:36:15 +0100

    gphotos-sync: fix SC2044 to handle files with spaces

--- gphotos-sync.sh
+++ gphotos-sync.sh
@@ -75,7 +75,7 @@ if [ "$DATE_LIMIT" ]; then
     FILES="$(ls -ht "$OUTPUT_DIR" | head -n 10000)"
 
     for d in $FILES; do
-        for f in $(find "$OUTPUT_DIR/$d" -iname "*jpg"); do
+        while IFS= read -r -d '' f; do
             EXIFDATE="$(exiftool -T -DateTimeOriginal "$f")"
             if [ $? != 0 ]; then
                 continue
@@ -87,7 +87,7 @@ if [ "$DATE_LIMIT" ]; then
                 URL='https://photos.google.com/photo/'"$d"
                 break
             fi
-        done
+        done < <(find "$OUTPUT_DIR/$d" -iname "*jpg" -print0)
         if [ -n "$URL" ]; then
             break
         fi
